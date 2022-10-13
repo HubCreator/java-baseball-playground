c320d824b3960768fda85590b0c6c5aa8313103a
HubCreator <chocomilk8604@gmail.com>
2022-10-13 18:39:49 +0900

    스트라이크와, 볼, 낫싱을 구분 로직 테스트

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,5 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class BaseballGame {
     public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
 
+        while (set.size() < 3) {
+            set.add(new Random().nextInt(9) + 1);
+        }
+        List<Integer> entry = new ArrayList<>(set);
+
+        while (true) {
+            String value = scanner.nextLine();
+            int[] result = howManyMatch(entry, value);
+        }
+    }
+
+    private static int[] howManyMatch(List<Integer> entry, String value) {
+        return new int[]{};
     }
 }
--- src/test/java/study/BaseballGameTest.java
+++ src/test/java/study/BaseballGameTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.*;
 class BaseballGameTest {
 
     @Test
-    void getThreeRandomNums() {
+    void getThreeRandomNumsTest() {
         Set<Integer> set = new HashSet<>();
         while (set.size() < 3) {
             set.add(new Random().nextInt(9) + 1);
@@ -20,5 +20,39 @@ class BaseballGameTest {
             assertThat(x).isBetween(1, 9);
 
         assertThat(set.size()).isEqualTo(3);
+    }
+
+    @Test
+    void howManyMatchTest() {
+        List<Integer> entry = new ArrayList<>();
+        entry.add(2);
+        entry.add(6);
+        entry.add(3);
+
+        String value1 = "214"; // 1스트라이크
+        String value2 = "213"; // 2스트라이크
+        String value3 = "613"; // 1볼 1스트라이크
+        String value4 = "145"; // 낫싱
+
+        assertThat(howManyMatch(entry, value1)).containsExactly(0, 1);
+        assertThat(howManyMatch(entry, value2)).containsExactly(0, 2);
+        assertThat(howManyMatch(entry, value3)).containsExactly(1, 1);
+        assertThat(howManyMatch(entry, value4)).containsExactly(0, 0);
+    }
+
+    /**
+     *
+     * @param entry
+     * @param value
+     * @return 볼과 스트라이크 순서대로 배열 반환
+     */
+    private int[] howManyMatch(List<Integer> entry, String value) {
+        int strike = 0, contains = 0;
+        for (int i = 0; i < value.length(); i++) {
+            int t = Integer.parseInt(String.valueOf(value.charAt(i)));
+            if (entry.get(i) == t) strike++;
+            if (entry.contains(t)) contains++;
+        }
+        return new int[]{contains - strike, strike};
     }
 }
