6624994235e8290bc0a98cc0611d39179e327183
HubCreator <chocomilk8604@gmail.com>
2022-10-13 17:39:59 +0900

    단위 테스트 실습 - 문자열 계산기 개선

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -9,19 +9,19 @@ public class StringCalculator {
         String value = scanner.nextLine();
         String[] values = value.split(" ");
 
-        List<String> num = new ArrayList<>();
+        List<Integer> num = new ArrayList<>();
         List<String> operation = new ArrayList<>();
         for (int i = 0; i < values.length; i++) {
             if (i % 2 == 1) operation.add(values[i]);
-            else if (i % 2 == 0) num.add(values[i]);
+            else if (i % 2 == 0) num.add(Integer.parseInt(values[i]));
         }
 
-        int answer = Integer.parseInt(num.get(0));
+        int answer = num.get(0);
         for (int i = 1; i < num.size(); i++) {
-            if (operation.get(i - 1).equals("+")) answer += Integer.parseInt(num.get(i));
-            else if (operation.get(i - 1).equals("-")) answer -= Integer.parseInt(num.get(i));
-            else if (operation.get(i - 1).equals("*")) answer *= Integer.parseInt(num.get(i));
-            else if (operation.get(i - 1).equals("/")) answer /= Integer.parseInt(num.get(i));
+            if (operation.get(i - 1).equals("+")) answer += num.get(i);
+            else if (operation.get(i - 1).equals("-")) answer -= num.get(i);
+            else if (operation.get(i - 1).equals("*")) answer *= num.get(i);
+            else if (operation.get(i - 1).equals("/")) answer /= num.get(i);
         }
 
         System.out.println("answer = " + answer);
