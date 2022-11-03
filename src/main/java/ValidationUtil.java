aa14261a291cee1b2ee4c924f9d0af9c64412da6
HubCreator <chocomilk8604@gmail.com>
2022-11-03 10:22:05 +0900

    feat : 중복된 숫자 제한

--- src/main/java/ValidationUtil.java
+++ src/main/java/ValidationUtil.java
@@ -1,9 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ValidationUtil {
-    public static boolean isValidInput(int val) {
+    public static boolean isValidRange(int val) {
         return val >= 1 && val <= 9;
     }
 
     public static boolean isSizeOfThree(int val) {
         return (int) Math.log10(val) + 1 == 3;
+    }
+
+    public static boolean hasDuplicatedNum(int val) {
+        int size = (int) Math.log10(val) + 1;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            set.add(val % 10);
+            val /= 10;
+        }
+        return set.size() == size;
     }
 }
--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -6,10 +6,10 @@ public class ValidationTest {
 
     @Test
     void rangeTest() {
-        assertThat(ValidationUtil.isValidInput(1)).isTrue();
-        assertThat(ValidationUtil.isValidInput(0)).isFalse();
-        assertThat(ValidationUtil.isValidInput(9)).isTrue();
-        assertThat(ValidationUtil.isValidInput(10)).isFalse();
+        assertThat(ValidationUtil.isValidRange(1)).isTrue();
+        assertThat(ValidationUtil.isValidRange(0)).isFalse();
+        assertThat(ValidationUtil.isValidRange(9)).isTrue();
+        assertThat(ValidationUtil.isValidRange(10)).isFalse();
     }
 
     @Test
@@ -17,5 +17,16 @@ public class ValidationTest {
         assertThat(ValidationUtil.isSizeOfThree(123)).isTrue();
         assertThat(ValidationUtil.isSizeOfThree(12)).isFalse();
         assertThat(ValidationUtil.isSizeOfThree(1234)).isFalse();
+    }
+
+    @Test
+    void duplicateTest() {
+        assertThat(ValidationUtil.hasDuplicatedNum(123)).isTrue();
+        assertThat(ValidationUtil.hasDuplicatedNum(12)).isTrue();
+        assertThat(ValidationUtil.hasDuplicatedNum(1)).isTrue();
+
+        assertThat(ValidationUtil.hasDuplicatedNum(122)).isFalse();
+        assertThat(ValidationUtil.hasDuplicatedNum(111)).isFalse();
+        assertThat(ValidationUtil.hasDuplicatedNum(1111)).isFalse();
     }
 }
